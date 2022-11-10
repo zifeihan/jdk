0d89d5e60f88becbe86b9044819865d82684eb51
zifeihan <caogui@iscas.ac.cn>
2022-11-10 22:52:55 +0800

    Polishing

--- start_jdk.sh
+++ start_jdk.sh
@@ -16,11 +16,11 @@ rm -rf build/
 
 make clean
 
-export CC=/home/dingli/toolchain-rvv/riscv64/bin/riscv64-unknown-linux-gnu-gcc
+export CC=/home/zifeihan/rvv/riscv64/bin/riscv64-unknown-linux-gnu-gcc
 
-export CXX=/home/dingli/toolchain-rvv/riscv64/bin/riscv64-unknown-linux-gnu-g++
+export CXX=/home/zifeihan/rvv/riscv64/bin/riscv64-unknown-linux-gnu-g++
 
-RVHOME=/home/dingli/toolchain-rvv/riscv64
+RVHOME=/home/zifeihan/rvv/riscv64
 JREPATH=/home/zifeihan/jre/jre19
 
 bash configure \
@@ -30,8 +30,8 @@ bash configure \
 --with-boot-jdk=${JREPATH} \
 --with-jvm-variants=server \
 --with-native-debug-symbols=internal \
---with-debug-level=fastdebug
+--with-debug-level=slowdebug
 
 make -j
 
-/home/zifeihan/rvv/qemu-7.1.0-rc4-riscv64/bin/qemu-riscv64 -cpu rv64,v=true,vlen=256,vext_spec=v1.0 -L /home/dingli/toolchain-rvv/riscv64/sysroot ./java -version
+/home/zifeihan/rvv/qemu-7.1.0-rc4-riscv64/bin/qemu-riscv64 -cpu rv64,v=true,vlen=256,vext_spec=v1.0 -L /home/zifeihan/rvv/riscv64/sysroot ./java -version
