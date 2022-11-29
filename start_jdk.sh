7bac1fec7d4c6887d52673b913bf32640cd5f653
zifeihan <caogui@iscas.ac.cn>
2022-11-29 22:10:18 +0800

    Polishing start_jdk.sh

--- start_jdk.sh
+++ start_jdk.sh
@@ -16,11 +16,7 @@ rm -rf build/
 
 make clean
 
-export CC=/home/zifeihan/rvv/riscv64/bin/riscv64-unknown-linux-gnu-gcc
-
-export CXX=/home/zifeihan/rvv/riscv64/bin/riscv64-unknown-linux-gnu-g++
-
-RVHOME=/home/zifeihan/rvv/riscv64
+RVHOME=/opt/riscv64
 JREPATH=/home/zifeihan/jre/jre19
 
 bash configure \
@@ -30,8 +26,8 @@ bash configure \
 --with-boot-jdk=${JREPATH} \
 --with-jvm-variants=server \
 --with-native-debug-symbols=internal \
---with-debug-level=slowdebug
+--with-debug-level=fastdebug
 
-make -j
+make JOBS=$(nproc)
 
-/home/zifeihan/rvv/qemu-7.1.0-rc4-riscv64/bin/qemu-riscv64 -cpu rv64,v=true,vlen=256,vext_spec=v1.0 -L /home/zifeihan/rvv/riscv64/sysroot ./java -version
+/home/zifeihan/rvv/qemu-7.1.0-rc4-riscv64/bin/qemu-riscv64 -cpu rv64,v=true,vlen=256,vext_spec=v1.0 -L /opt/riscv64/sysroot ./java -version
