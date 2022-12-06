044eef55678baf6ac10e28b3eca81a8870d2ac9a
zifeihan <caogui@iscas.ac.cn>
2022-12-06 16:32:31 +0800

    Polishing

--- start_jdk.sh
+++ start_jdk.sh
@@ -28,6 +28,6 @@ bash configure \
 --with-native-debug-symbols=internal \
 --with-debug-level=fastdebug
 
-make JOBS=$(nproc)
+make all
 
 /home/zifeihan/rvv/qemu-7.1.0-rc4-riscv64/bin/qemu-riscv64 -cpu rv64,v=true,vlen=256,vext_spec=v1.0 -L /opt/riscv64/sysroot ./java -version
